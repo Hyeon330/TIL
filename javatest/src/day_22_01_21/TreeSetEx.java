bc35b256bb58587bed38d09c0cde66e99ae63448
이현수 <dldlsrn231@gmail.com>
2022-01-22 21:04:47 +0900

    update

---
+++ javatest/src/day_22_01_21/TreeSetEx.java
@@ -0,0 +1,48 @@
+package day_22_01_21;
+
+import java.util.*;
+
+public class TreeSetEx {
+}
+
+class TreeSetEx1 {
+    public static void main(String[] args) {
+        Set set = new TreeSet<>();
+
+        for (int i = 0; set.size() < 6; i++) {
+            int num = (int) (Math.random() * 45) + 1;
+            set.add(num); // set.add(new Integer(num));
+        }
+
+        System.out.println(set);
+    }
+}
+
+class TreeSetEx2 {
+    public static void main(String[] args) {
+        TreeSet set = new TreeSet<>();
+
+        String from = "b";
+        String to = "d";
+
+        set.add("abc");
+        set.add("alien");
+        set.add("bat");
+        set.add("car");
+        set.add("Car");
+        set.add("disc");
+        set.add("dance");
+        set.add("dZZZZ");
+        set.add("dzzzz");
+        set.add("elephant");
+        set.add("elevator");
+        set.add("fan");
+        set.add("flower");
+
+        System.out.println(set);
+
+        System.out.println("range search : from " + from + " to " + to);
+        System.out.println("result1 : " + set.subSet(from, to));
+        System.out.println("result2 : " + set.subSet(from, to + "zzz"));
+    }
+}
