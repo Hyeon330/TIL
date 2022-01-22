df98fb8d7ef7e2dc3c4e1ca3366cdd5d764217f0
이현수 <dldlsrn231@gmail.com>
2022-01-22 20:20:40 +0900

    update

---
+++ javatest/src/day_22_01_21/HashSetEx.java
@@ -0,0 +1,80 @@
+package day_22_01_21;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class HashSetEx {
+}
+
+class hashSetEx1 {
+    public static void main(String[] args) {
+        Object[] objArr = { "1", new Integer(1), "2", "2", "3", "3", "4", "4", "4" };
+        Set set = new HashSet();
+
+        for (int i = 0; i < objArr.length; i++) {
+            set.add(objArr[i]); // hashSet에 objArr의 요소들을 저장한다.
+        }
+
+        System.out.println(set);
+    }
+}
+
+class HashSetEx2 {
+    public static void main(String[] args) {
+        Set set = new HashSet<>();
+
+        for (int i = 0; set.size() < 6; i++) {
+            int num = (int) (Math.random() * 45) + 1;
+            set.add(new Integer(num));
+        }
+
+        List list = new LinkedList(set);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
+
+class HashSetEx3 {
+    public static void main(String[] args) {
+        HashSet set = new HashSet();
+
+        set.add(new String("abc"));
+        set.add(new String("abc"));
+        set.add(new Person("David", 10));
+        set.add(new Person("David", 10));
+
+        System.out.println(set);
+    }
+}
+
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Person) {
+            Person tmp = (Person) obj;
+            return name.equals(tmp.name) && age == tmp.age;
+        }
+
+        return false;
+    }
+
+    public int hashCode() {
+        return (name + age).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return name + ":" + age;
+    }
+}
