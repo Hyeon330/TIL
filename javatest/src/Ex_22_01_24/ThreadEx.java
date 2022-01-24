ae29cd4e0e7e7329275b8cc661c5f4a57306e04e
이현수 <dldlsrn231@gmail.com>
2022-01-25 03:25:31 +0900

    update

--- javatest/src/Ex_22_01_24/ThreadEx.java
+++ javatest/src/Ex_22_01_24/ThreadEx.java
@@ -1,5 +1,9 @@
 package Ex_22_01_24;
 
+import java.util.Iterator;
+import java.util.Map;
+import javax.swing.JOptionPane;
+
 public class ThreadEx {
 
 }
@@ -83,7 +87,8 @@ class ThreadEx5 {
         for (int i = 0; i < 300; i++) {
             System.out.printf("%s", new String("-"));
         }
-        System.out.print("소요시간1:" + (System.currentTimeMillis() - ThreadEx5.startTime));
+        System.out.print("소요시간1:" + (System.currentTimeMillis() -
+                ThreadEx5.startTime));
     }
 }
 
@@ -92,6 +97,253 @@ class ThreadEx5_1 extends Thread {
         for (int i = 0; i < 300; i++) {
             System.out.printf("%s", new String("|"));
         }
-        System.out.print("소요시간2:" + (System.currentTimeMillis() - ThreadEx5.startTime));
+        System.out.print("소요시간2:" + (System.currentTimeMillis() -
+                ThreadEx5.startTime));
+    }
+}
+
+class ThreadEx8 {
+    public static void main(String[] args) {
+        ThreadEx8_1 th1 = new ThreadEx8_1();
+        ThreadEx8_2 th2 = new ThreadEx8_2();
+
+        th2.setPriority(7);
+
+        System.out.println("Priority of th1(-) : " + th1.getPriority());
+        System.out.println("Priority of th2(|) : " + th2.getPriority());
+        th1.start();
+        th2.start();
+    }
+}
+
+class ThreadEx8_1 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 300; i++) {
+            System.out.print("-");
+            for (int j = 0; j < 10000000; j++)
+                ;
+        }
+    }
+}
+
+class ThreadEx8_2 extends Thread {
+    public void run() {
+        for (int i = 0; i < 300; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 10000000; j++)
+                ;
+        }
+    }
+}
+
+class ThreadEx9 {
+    public static void main(String[] args) throws Exception {
+        ThreadGroup main = Thread.currentThread().getThreadGroup();
+        ThreadGroup grp1 = new ThreadGroup("Group1");
+        ThreadGroup grp2 = new ThreadGroup("Group2");
+
+        // ThreadGroup(ThreadGroup parent, String name)
+        ThreadGroup subGrp1 = new ThreadGroup(grp1, "SubGroup1");
+
+        grp1.setMaxPriority(3);
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000); // 쓰레드를 1초간 멈춘다.
+                } catch (InterruptedException e) {
+                    // TODO: handle exception
+                }
+            }
+        };
+        // Thread(ThreadGroup tg, Runnable r, String name)
+        new Thread(grp1, r, "th1").start();
+        new Thread(subGrp1, r, "th2").start();
+        new Thread(grp2, r, "th3").start();
+
+        System.out.println(">>List of ThreadGroup : " + main.getName()
+                + ", Active ThreadGroup: " + main.activeGroupCount()
+                + ", Active Thread: " + main.activeCount());
+        main.list();
+    }
+}
+
+class ThreadEx10 implements Runnable {
+    static boolean autoSave = false;
+
+    public static void main(String[] args) {
+        Thread t = new Thread(new ThreadEx10());
+        t.setDaemon(true); // 이 부분이 없으면 종료되지 않음
+        t.start();
+
+        for (int i = 1; i <= 10; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            System.out.println(i);
+
+            if (i == 5)
+                autoSave = true;
+
+        }
+        System.out.println("프로그램을 종료합니다.");
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(3 * 1000); // 3초마다
+            } catch (InterruptedException e) {
+            }
+            // autoSave의 값이 true이면 autoSave()를 호출한다.
+            if (autoSave) {
+                autoSave();
+            }
+        }
+    }
+
+    private void autoSave() {
+        System.out.println("작업파일이 자동저장되었습니다.");
+    }
+}
+
+class ThreadEx11 {
+    public static void main(String[] args) {
+        ThreadEx11_1 t1 = new ThreadEx11_1("Thread1");
+        ThreadEx11_2 t2 = new ThreadEx11_2("Thread2");
+        t1.start();
+        t2.start();
+    }
+}
+
+class ThreadEx11_1 extends Thread {
+    ThreadEx11_1(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        try {
+            sleep(5 * 1000);
+        } catch (InterruptedException e) {
+        }
+    }
+}
+
+class ThreadEx11_2 extends Thread {
+    ThreadEx11_2(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        Map map = getAllStackTraces();
+        Iterator it = map.keySet().iterator();
+
+        int x = 0;
+        while (it.hasNext()) {
+            Object obj = it.next();
+            Thread t = (Thread) obj;
+            StackTraceElement[] ste = (StackTraceElement[]) (map.get(obj));
+
+            System.out.println("[" + ++x + "] name : " + t.getName() + ", group : " + t.getThreadGroup().getName()
+                    + ", daemon : " + t.isDaemon());
+
+            for (int i = 0; i < ste.length; i++) {
+                System.out.println(ste[i]);
+            }
+            System.out.println();
+        }
+    }
+}
+
+class ThreadEx12 {
+    public static void main(String[] args) {
+        ThreadEx12_1 th1 = new ThreadEx12_1();
+        ThreadEx12_2 th2 = new ThreadEx12_2();
+        th1.start();
+        th2.start();
+
+        try {
+            th1.sleep(2000);
+        } catch (InterruptedException e) {
+        }
+        System.out.println("<<main 종료>>");
+    }
+}
+
+class ThreadEx12_1 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 300; i++)
+            System.out.print("-");
+        System.out.print("<<th1 종료>>");
+    }
+}
+
+class ThreadEx12_2 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 300; i++)
+            System.out.print("|");
+        System.out.print("<<th2 종료>>");
+    }
+}
+
+class ThreadEx13 {
+    public static void main(String[] args) throws Exception {
+        ThreadEx13_1 th1 = new ThreadEx13_1();
+        th1.start();
+        String input = JOptionPane.showInputDialog("아무값이나 입력하세요");
+        System.out.println("입력하신 값은 " + input + "입니다.");
+        th1.interrupt(); // interrupt()를 호출하면, interrupted상태가 true가 된다.
+        System.out.println("isInterrupted():" + th1.isInterrupted()); // true
+    }
+}
+
+class ThreadEx13_1 extends Thread {
+    @Override
+    public void run() {
+        int i = 10;
+
+        while (i != 0 && !isInterrupted()) {
+            System.out.println(i--);
+            for (Long x = 0L; x < 2500000000L; x++)
+                ; // 시간지연
+        }
+        System.out.println("카운트가 종료되었습니다.");
+    }
+}
+
+class ThreadEx14 {
+    public static void main(String[] args) {
+        ThreadEx14_1 th1 = new ThreadEx14_1();
+        th1.start();
+
+        String input = JOptionPane.showInputDialog("아무 값이나 입력하세요.");
+        System.out.println("입력하신 값은 " + input + "입니다.");
+        th1.interrupt(); // interrupt()를 호출하면, interrupted상태가 true가 된다.
+        System.out.println("isInterrupted():" + th1.isInterrupted());
+    }
+}
+
+class ThreadEx14_1 extends Thread {
+    @Override
+    public void run() {
+        int i = 10;
+
+        while (i != 0 && !isInterrupted()) {
+            System.out.println(i--);
+            try {
+                Thread.sleep(1000); // 1초 지연
+            } catch (InterruptedException e) {
+                interrupt();
+            }
+        }
+        System.out.println("카운트가 종료되었습니다.");
     }
 }
