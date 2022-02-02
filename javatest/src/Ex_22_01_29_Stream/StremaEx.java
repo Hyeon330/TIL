9f4f65ea67f7ac806f81978a7a281fb9c4b8463d
이현수 <dldlsrn231@gmail.com>
2022-02-02 21:32:02 +0900

    update

--- javatest/src/Ex_22_01_29_Stream/StremaEx.java
+++ javatest/src/Ex_22_01_29_Stream/StremaEx.java
@@ -3,32 +3,32 @@ package Ex_22_01_29_Stream;
 import java.util.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.*;
-import static java.util.Comparator.*;
 
 class StreamEx6 {
     public static void main(String[] args) {
-        Student[] stuArr = {
-                new Student("이자바", 3, 300),
-                new Student("김자바", 1, 200),
-                new Student("안자바", 2, 100),
-                new Student("박자바", 2, 150),
-                new Student("소자바", 1, 200),
-                new Student("나자바", 3, 290),
-                new Student("김자바", 3, 180)
+        Student_[] stuArr = {
+                new Student_("이자바", 3, 300),
+                new Student_("김자바", 1, 200),
+                new Student_("안자바", 2, 100),
+                new Student_("박자바", 2, 150),
+                new Student_("소자바", 1, 200),
+                new Student_("나자바", 3, 290),
+                new Student_("김자바", 3, 180)
         };
 
         // 학생 이름만 뽑아서 List에 저장
-        List<String> names = Stream.of(stuArr).map(Student::getName)
+        List<String> names = Stream.of(stuArr).map(Student_::getName)
                 .collect(Collectors.toList());
         System.out.println(names);
 
         // 스트림을 배열로 변환
-        Student[] stuArr2 = Stream.of(stuArr).toArray(Student[]::new);
+        Student_[] stuArr2 = Stream.of(stuArr).toArray(Student_[]::new);
 
-        for (Student s : stuArr2)
+        for (Student_ s : stuArr2)
             System.out.println(s);
 
-        // Map<String, Student> stuMap = Stream.of(stuArr)
+        // // 스트림을 Map<String, Student_>로 변환. 학생 이름이 Key
+        // Map<String, Student_> stuMap = Stream.of(stuArr)
         // .collect(Collectors.toMap(s -> s.getName(), p -> p));
 
         // for (String name : stuMap.keySet())
@@ -36,36 +36,36 @@ class StreamEx6 {
 
         long count = Stream.of(stuArr).collect(counting());
         long totalScore = Stream.of(stuArr)
-                .collect(summingInt(Student::getTotalScore));
+                .collect(summingInt(Student_::getTotalScore));
         System.out.println("count=" + count);
         System.out.println("totalScore=" + totalScore);
 
         totalScore = Stream.of(stuArr)
-                .collect(reducing(0, Student::getTotalScore, Integer::sum));
+                .collect(reducing(0, Student_::getTotalScore, Integer::sum));
         System.out.println("totalScore=" + totalScore);
 
-        Optional<Student> topStudent = Stream.of(stuArr)
-                .collect(maxBy(Comparator.comparingInt(Student::getTotalScore)));
-        System.out.println("topStudent=" + topStudent.get());
+        Optional<Student_> topStudent_ = Stream.of(stuArr)
+                .collect(maxBy(Comparator.comparingInt(Student_::getTotalScore)));
+        System.out.println("topStudent_=" + topStudent_.get());
 
         IntSummaryStatistics stat = Stream.of(stuArr)
-                .collect(summarizingInt(Student::getTotalScore));
+                .collect(summarizingInt(Student_::getTotalScore));
 
         System.out.println(stat);
 
-        String stuNames = Stream.of(stuArr).map(Student::getName)
+        String stuNames = Stream.of(stuArr).map(Student_::getName)
                 .collect(joining(",", "{", "}"));
 
         System.out.println(stuNames);
     }
 }
 
-class Student implements Comparable<Student> {
+class Student_ implements Comparable<Student_> {
     String name;
     int ban;
     int totalScore;
 
-    Student(String name, int ban, int totalScore) {
+    Student_(String name, int ban, int totalScore) {
         this.name = name;
         this.ban = ban;
         this.totalScore = totalScore;
@@ -89,7 +89,7 @@ class Student implements Comparable<Student> {
     }
 
     @Override
-    public int compareTo(Student s) {
+    public int compareTo(Student_ s) {
         return s.totalScore - this.totalScore;
     }
 }
