0abef16e48d9c31877a77b3e2b8af4b261bd11d3
hyeon3 <dldlsrn231@gmail.com>
2022-05-27 05:20:25 +0900

    update

---
+++ Kotlin/Kotlin_TIL/src/main/kotlin/chap02/section2/FormattedString.kt
@@ -0,0 +1,11 @@
+package chap02.section2
+
+fun main() {
+    val num = 10
+    val formattedString = """
+        Hello       World!!
+        abcd        efgh
+    """.trimIndent()
+
+    println(formattedString)
+}
